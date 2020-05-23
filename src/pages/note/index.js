688a050caad38f31bd515b7058c130ef38157840
szy1000 <1538078053@qq.com>
2020-05-23 13:42:37 +0800

    add note

---
+++ src/pages/note/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {Button, Text, View, TextInput, StyleSheet} from 'react-native';
+// import {Tab} from '../../components';
+import {Link} from '@react-navigation/native';
+
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+
+const TopTab = createMaterialTopTabNavigator();
+
+const Item1 = () => (
+  <View>
+    <Text>Item1</Text>
+  </View>
+);
+
+const Item2 = () => (
+  <View>
+    <Text>Item2</Text>
+  </View>
+);
+
+export default class BBS extends React.Component {
+  render() {
+    const {navigation} = this.props;
+    return (
+      <View>
+        <Text style={styles.text}>Welcome To Page BBS</Text>
+        <Link to={'/Register'}>sss</Link>
+
+        <TextInput
+          style={{
+            width: 200,
+            height: 40,
+          }}
+          placeholder="请输入"
+          onChangeText={text => {
+            navigation.setParams({
+              iTitle: text,
+            });
+          }}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    // color: '#ddd',
+  },
+});
